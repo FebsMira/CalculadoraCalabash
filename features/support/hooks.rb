05d87f93525ae6ca00d5ef418d80951c30f5fcd2
Felipe Mira <felipemira@Felipes-MacBook-Pro.local>
2017-07-09 00:51:17 -0300

    redimensionando imagem de evidencia

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -14,14 +14,19 @@ After do |scenario|
 
 	sufix = ('error' if scenario.failed?) || 'sucess'
 	name = scenario.name.tr(' ','_').downcase
-
-	screenshot_embed(:prefix => nil,:name => "./report/#{sufix}_#{name}_#{day}-#{month}-#{year}")
 	
-	image = MiniMagick::Image.open("./report/#{sufix}_#{name}_#{day}-#{month}-#{year}png")
+	screenshot_embed(:name => "./report/#{sufix}_#{name}_#{day}-#{month}-#{year}")
+
+	#Lista os arquivos mais recentes de uma determinada pasta, para acessar o mais recente e so colocar dir[-1]
+	dir = Dir.glob("./report/*").sort_by { |f| File.mtime(f) }
+	#puts dir[-1]
+
+	#redimensiona a imagem
+	image = MiniMagick::Image.open(dir[-1])
 	image.path #=> "./report/"
 	image.resize "300x650"
 	image.format "png"
-	image.write "./report/#{sufix}_#{name}_#{day}-#{month}-#{year}.png"
+	image.write dir[-1]
 
     shutdown_test_server
 
